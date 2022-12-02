af71b97887c6d12e22b8910e0289e9bbfb1e2256
samyukta hariharan <sammie1999@gmail.com>
2022-12-02 16:04:38 +0800

    Converting raw review text to word bigrams

--- preprocessing.py
+++ preprocessing.py
@@ -1,13 +1,48 @@
-import pandas as pd
+# Import Packages
 import nltk
+import string
+import pandas as pd
+from nltk.util import ngrams
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import word_tokenize
 
+# Download nltk packages for specific preprocessing tasks
 nltk.download('punkt')
-reviews = pd.read_csv('data\\reviews.csv')
+nltk.download('wordnet') 
+nltk.download('omw-1.4')
+nltk.download("stopwords")
 
-reviews.info()
+# Read parquet file into pandas dataframe
+reviews = pd.read_parquet('data\\reviews_raw.parquet')
 
+# Define lemmatization and stopwords objects
+lemmatizer = WordNetLemmatizer()
+stopwords = stopwords.words('english')
+
+# Convert reviewsText column to string
 reviews["reviewText"] = reviews["reviewText"].astype('str')
-reviews["reviewTokens"] = reviews["reviewText"].apply(word_tokenize)
 
-# reviews.head(10)
+# Create duplicate reviewText column called reviewTokens to perform preprocessing
+reviews["reviewTokens"] = reviews["reviewText"]
+
+# Convert all words in each review to lowercase
+reviews["reviewTokens"] = reviews["reviewTokens"].apply(lambda a: a.lower())
+
+# Remove all punctuations from each review
+reviews["reviewTokens"] = reviews["reviewTokens"].str.replace('[{}]'.format(string.punctuation), '')
+
+# Remove all stopwords from each review
+reviews["reviewTokens"] = reviews["reviewTokens"].apply(lambda x: ' '.join([word for word in x.split() if word not in (stopwords)]))
+
+# Perform lemmatization on each word of each review
+reviews["reviewTokens"] = reviews["reviewTokens"].apply(lambda a: lemmatizer.lemmatize(a))
+
+# Split the words in each review into a list of words by tokenization
+reviews["reviewTokens"] = reviews["reviewTokens"].apply(word_tokenize)
+
+# Combine words occuring together as bigrams in a last for each review
+reviews['reviewTokens'] = reviews["reviewTokens"].apply(lambda row: list(nltk.ngrams(row, 2))) 
+
+# Save preprocessed file with reviewTokens column to parquet file
+reviews.to_parquet("data\\reviews_preprocessed.parquet")
--- read_data.py
+++ read_data.py
@@ -1,11 +1,14 @@
-import pandas as pd
+# Import packages
 import gzip
+import pandas as pd
 
+# Function to parse the .json.gz file
 def parse(path):
   g = gzip.open(path, 'rb')
   for l in g:
     yield eval(l)
 
+# Dunction to convert the .json.gz file into pandas dataframe
 def getDF(path):
   i = 0
   df = {}
@@ -14,5 +17,8 @@ def getDF(path):
     i += 1
   return pd.DataFrame.from_dict(df, orient='index')
 
+# Calling the function into a dataframe object
 df = getDF('data\\reviews_Cell_Phones_and_Accessories_5.json.gz')
-df.to_parquet('data\\reviews.parquet',index=False)
+
+# Saving the dataframe to parquet file
+df.to_parquet('data\\reviews_raw.parquet',index=False)
